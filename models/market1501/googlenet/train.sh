83e401f62865dc5fe370b8920aac97b7727a554d
D-X-Y <dongxuanyi888@icloud.com>
2017-12-02 02:51:18 +1100

    fix googlenet script

--- models/market1501/googlenet/train.sh
+++ models/market1501/googlenet/train.sh
@@ -2,4 +2,4 @@
 model_dir=models/market1501/googlenet
 pre_train_dir=${HOME}/datasets/model_pretrained/googlenet
 
-GLOG_log_dir=${model_dir}/log ./build/tools/caffe train --solver ${model_dir}/solver.proto --weights ${pre_train_dir}/bvlc_googlenet.caffemodel
+GLOG_log_dir=${model_dir}/log ./build/tools/caffe train --solver ${model_dir}/solver.proto --weights ${pre_train_dir}/bvlc_googlenet.caffemodel $@
